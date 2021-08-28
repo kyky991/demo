6c465bf5416895eae488ced17cd8c4fa64f80855
Zing <cyunzing@gmail.com>
2021-08-28 11:24:03 +0800

    event loop group

--- springboot/service/test/src/test/java/com/zing/test/netty/echo/EchoServer.java
+++ springboot/service/test/src/test/java/com/zing/test/netty/echo/EchoServer.java
@@ -2,6 +2,7 @@ package com.zing.test.netty.echo;
 
 import com.zing.test.netty.Server;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
 import static com.zing.test.netty.Constants.PORT;
@@ -11,11 +12,14 @@ public class EchoServer {
     public static void main(String[] args) throws Exception {
         EchoServerHandler serverHandler = new EchoServerHandler();
 
+        // 此方式只会使用一个线程去处理，适合客户端连接多、业务处理时间短的场景
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
         new Server()
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel ch) throws Exception {
-                        ch.pipeline().addLast(serverHandler);
+                        ch.pipeline().addLast(group, serverHandler);
                     }
                 })
                 .port(PORT)
