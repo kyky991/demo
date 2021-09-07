76b6a53f6266d295a16277e3cfc20462e26a998a
Zing <cyunzing@gmail.com>
2021-09-08 00:10:13 +0800

    test

--- springboot/service/test/src/test/java/Test.java
+++ springboot/service/test/src/test/java/Test.java
@@ -1,8 +1,48 @@
-public class Test {
 
-    public static void main(String[] args) {
-        String name = "main";
-        System.out.println(name);
+class Foo {
+    int i = 1;
+
+    Foo() {
+        System.out.println(i);
+        int x = getValue();
+        System.out.println(x);
     }
 
+    {
+        i = 2;
+    }
+
+    protected int getValue() {
+        return i;
+    }
+}
+
+//子类
+class Bar extends Foo {
+    int j = 1;
+
+    Bar() {
+        j = 2;
+    }
+
+    {
+        j = 3;
+    }
+
+    @Override
+    protected int getValue() {
+        return j;
+    }
+}
+
+public class Test {
+    public static void main(String... args) {
+        /*
+        2
+        0
+        2
+         */
+        Bar bar = new Bar();
+        System.out.println(bar.getValue());
+    }
 }
